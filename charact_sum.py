a070e93a0894548f430c464d78362102f60da309
pragatirahul123 <47949770+pragatirahul123@users.noreply.github.com>
2021-04-27 12:33:30 +0530

    Update charact_sum.py

--- charact_sum.py
+++ charact_sum.py
@@ -3,7 +3,7 @@ list2=["y","me","s","na"]
 index=0
 new=[]
 while index<len(list1):
-     var=list1[i]+list2[i]
+     var=list1[index]+list2[index]
      new.append(var)
      index=index+1
 print(new)
